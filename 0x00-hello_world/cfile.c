13f576992ab64852a167705d304ea412ab2003be
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-10 13:28:07 -0700

    commit

--- 0x00-hello_world/cfile.c
+++ 0x00-hello_world/cfile.c
@@ -6,9 +6,11 @@
  * Description: A program that prints using the puts function
  *
  * Return: Always 0 (Success)
- * */
+ *
+*/
 
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
