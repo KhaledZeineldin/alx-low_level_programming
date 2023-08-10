e11fa7480f2a2e96097b20f76bdecf0419e74ed0
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-10 14:03:12 -0700

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints using the puts function
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	char[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	for (int i = 0; i < 59; i++)
+{
+	putchar(char[i]);
+}
+putchar("\n");
+return (1);
+}
