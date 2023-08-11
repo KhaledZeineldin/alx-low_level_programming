d1b181bf5133754b3f493af69d7104cf9878cbe1
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-11 01:18:42 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that tells if a number's
+ * units digit is greater than of less than five
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+for (int a = 0; a <= 9; a++)
+{
+	printf("%d", a);
+}
+	printf("\n");
+	return (0);
+}
