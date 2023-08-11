73ae445de5b5ffb745c582b19cb98a189e88634f
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-11 06:28:00 -0700

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -16,10 +16,11 @@ int main(void)
 	int a;
 for (a = 'a'; a <= 'z'; a++)
 {
-	if (a != 'q' && a != e)
+	if (a != 'q' && a != 'e')
 {
 	putchar(a);
 }
 }
-	putchar("\n");
+	putchar('\n');
+	return (0);
 }
