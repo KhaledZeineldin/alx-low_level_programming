ad6e6e34b0847018248d62256139024546bef34a
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-11 01:15:29 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that tells if a number's
+ * units digit is greater than of less than five
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+for (int a = 'a'; a <= 'z'; a++)
+{
+	if (a != 'q' && a != e)
+{
+	putchar(a);
+}
+}
+	putchar("\n");
+}
