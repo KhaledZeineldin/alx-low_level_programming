a44c2d716bc8b5e1b190b099caaf86976c9c3bc4
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-11 00:48:06 -0700

    commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints the sign of the input number
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n >= 0)
+	{printf("%dis positive", n);
+	}
+	if (n == 0)
+	{printf("%dis zero", n);
+	}
+	else
+	{printf("%dis negative", n);
+	return (0);
+}
