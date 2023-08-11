8154c863c0486afbd5a923774ae65f32ca554ad6
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-11 06:49:02 -0700

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,12 +15,15 @@
 int main(void)
 {
 	int a;
-for (a = 0; a < 9; a++)
+for (a = 0; a <= 9; a++)
 {
 	putchar(a + '0');
-	putchar(',');
-	putchar(' ');
+	if (a != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
 }
-	putchar(a + '0');
+	putchar('\n');
 	return (0);
 }
