9ec89802447ffd41401c936f076f15f60aa1c310
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-15 05:26:29 -0700

    commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * abs - prints 1 if character is lower case else 0
+ * _abs - prints 1 if character is lower case else 0
  * Description - tells if a character is lower case or not
  * @n: the character number according to ASCII
  * Return: Always 0 (Success)
