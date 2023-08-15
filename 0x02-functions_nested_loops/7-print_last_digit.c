915b3c4742064b4a6afb1950d9aafd8e27822e29
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-15 05:35:03 -0700

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - prints 1 if character is lower case else 0
+ * Description - tells if a character is lower case or not
+ * @n: the character number according to ASCII
+ * Return: Always 0 (Success)
+ *
+*/
+int print_last_digit(int n)
+{
+return (n % 10);
+}
