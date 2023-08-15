83d86983fcdb78e977d567163d3d1d4fc836e0a0
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-15 07:45:44 -0700

    commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - prints 1 if character is lower case else 0
+ * Description - tells if a character is lower case or not
+ * Return: Always 0 (Success)
+ *
+*/
+void times_table(void)
+{
+int i, j = 0;
+for (int i =0; i <= 9; i++)
+{
+_putchar(i + 48);
+if (i == 9)
+{
+j =0;
+continue;
+}
+_putchar(',');
+_putchar(' ');
+}
+
+}
