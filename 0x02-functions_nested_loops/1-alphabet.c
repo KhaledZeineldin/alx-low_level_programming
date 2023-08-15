94e0cf3a4df4fef1e82cd2f0da4bd88326a42a38
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-15 04:38:56 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints alphabets
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+return (0);
+}
