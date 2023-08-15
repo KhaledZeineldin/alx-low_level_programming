bc084d9f59b4899041423cc47dbcb7c6ca40d2b3
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-15 04:49:28 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets ten times
+ *
+ * Description: A program that tells if a number's
+ * units digit is greater than of less than five
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	for (i = 0; i <=9 ; i++)
+{
+	print_alphabet();
+}
+}
