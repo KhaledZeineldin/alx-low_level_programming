53bbc1ff3deb5ed0750bace1c188020e14bebdda
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-16 23:15:54 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_isupper - function that tells if a character is upper case or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
+int _isupper(int c)
+{
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+
+return (0);
+}
