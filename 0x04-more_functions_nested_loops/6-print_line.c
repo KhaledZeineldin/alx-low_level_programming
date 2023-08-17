2121216f900aa9c55092b4a5382ef96b63595b77
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-17 13:35:42 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
+void print_line(int n)
+{
+while (n--)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
