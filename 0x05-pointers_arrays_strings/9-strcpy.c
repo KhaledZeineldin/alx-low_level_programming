d13f1e497528d6b37ebac8d4ed17f4295e129d69
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-08-21 08:25:01 -0700

    commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,15 +8,11 @@
 char *_strcpy(char *dest, char *src)
 {
 int i = 0;
-while(1)
+while (*(src + i) != '\0')
 {
 *(dest + i) = *(src + i);
 i++;
-if (*(src + i) == '\0')
-{
+}
 *(dest + i) = '\0';
-return (dest);
-}
-}
 return (dest);
 }
