40c86a429f59c4b2fe27b21a233e0fbb5d3a6eea
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-09-01 12:21:09 -0700

    hands crossed

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,18 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints the sign of the input number
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker\""
+_puts("\t- Kevin Mitnick");
+return (0);
 }
 
