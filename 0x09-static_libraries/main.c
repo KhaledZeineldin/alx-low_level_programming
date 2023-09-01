1afa3175ccaf750b75f526863b5df1c90a8fb59e
KhaledZeineldin <khaled.zein.new@gmail.com>
2023-09-01 12:27:46 -0700

    new

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\""
+_puts("\"At the end of the day, my goal was to be the best hacker\"";
 _puts("\t- Kevin Mitnick");
 return (0);
 }
